384a24a7fd3703d2afc303f452742adb866b92f9
Fabrice Le Fessant <fabrice.le_fessant@origin-labs.com>
2021-12-04 12:35:36 +0100

    Document lib/util/secure.ml and bin/gwrepl/data.ml

--- lib/util/secure.ml
+++ lib/util/secure.ml
@@ -9,6 +9,8 @@ let ok_r = ref []
 let assets_r = ref []
 let bd_r = ref Filename.current_dir_name
 
+(* [decompose: string -> string list] decompose a path into a list of
+   directory and a basename. "a/b/c" -> [ "a" ; "b"; "c" ] *)
 let decompose =
   let rec loop r s =
     let b = Filename.basename s in
@@ -32,9 +34,11 @@ let set_base_dir d =
   ok_r := ok :: (List.filter ((<>) ok)) !ok_r
 
 let assets () = !assets_r
-let bd () = !bd_r
+let base_dir () = !bd_r
 
-let suffix d df =
+(* [list_check_prefix d df] returns either [None] if [d] is not a prefix of
+   [df], or [Some suffix], where [df = d @ suffix] *)
+let list_check_prefix d df =
   let rec loop =
     function
       x :: xl, y :: yl -> if x = y then loop (xl, yl) else None
@@ -43,13 +47,19 @@ let suffix d df =
   in
   loop (d, df)
 
+(** Check if a filename is safe to read:
+    * it must not contain the '\000' character
+    * it must either be relative to the local directory OR
+      included in one of the allowed directories (base_dir or assets)
+    * the relative part does not contain the '..' directory
+*)
 let check fname =
   if String.contains fname '\000' then false
   else
     let df = decompose fname in
     let rec loop = function
       | d :: dl ->
-        begin match suffix d df with
+        begin match list_check_prefix d df with
           | Some bf -> not (List.mem Filename.parent_dir_name bf)
           | None -> loop dl
         end
@@ -60,7 +70,7 @@ let check fname =
     in
     loop !ok_r
 
-let check_open fname =
+let check_open do_open fname =
   if not (check fname) then begin
     if Sys.unix then
       begin
@@ -68,11 +78,14 @@ let check_open fname =
         flush stderr
       end;
     raise (Sys_error "invalid access")
-  end
+  end ;
+  do_open fname
 
-let open_in fname = check_open fname; Stdlib.open_in fname
-let open_in_bin fname = check_open fname; Stdlib.open_in_bin fname
-let open_out fname = check_open fname; Stdlib.open_out fname
-let open_out_bin fname = check_open fname; Stdlib.open_out_bin fname
-let open_out_gen mode perm fname =
-  check_open fname; Stdlib.open_out_gen mode perm fname
+(* The following functions perform a [check] before opening the file,
+   preventing potential attacks on the system.
+*)
+let open_in = check_open Stdlib.open_in
+let open_in_bin = check_open Stdlib.open_in_bin
+let open_out = check_open Stdlib.open_out
+let open_out_bin = check_open Stdlib.open_out_bin
+let open_out_gen mode perm = check_open (Stdlib.open_out_gen mode perm)
--- lib/util/secure.mli
+++ lib/util/secure.mli
@@ -1,16 +1,26 @@
 (* Copyright (c) 1998-2007 INRIA *)
 
+(* This module controls which directories can be read by the webserver, to
+   prevent malicious users from reading/writing on the server disk. *)
+
+(* Allowed directories are composed of [assets] and the [base_dir]. *)
 val assets : unit -> string list
-val bd : unit -> string
+val base_dir : unit -> string
 
 val add_assets : string -> unit
 val set_base_dir : string -> unit
 
-(** Check if a filename is safe to read or not
-    (i.e. will not read in a location it is not supposed to read).
+(** Check if a filename is safe to read:
+    * it must not contain the '\000' character
+    * it must either be relative to the local directory OR
+      included in one of the allowed directories (base_dir or assets)
+    * the relative part does not contain the '..' directory
 *)
 val check : string -> bool
 
+(* The following functions perform a [check] before opening the file,
+   preventing potential attacks on the system.
+*)
 val open_in : string -> in_channel
 val open_in_bin : string -> in_channel
 val open_out : string -> out_channel
